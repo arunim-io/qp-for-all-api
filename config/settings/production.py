0ce68d94c5909f06465f91667e0ca7eb3ac6583d
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-10-28 13:57:31 +0600

    prepare for deployment to render.com

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,15 +1,18 @@
+import dj_database_url
+from decouple import config
+
 from .main import ALLOWED_HOSTS, BASE_DIR
 
-ALLOWED_HOSTS += ["*"]
+ALLOWED_HOSTS += [config("RENDER_EXTERNAL_HOSTNAME")]
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgresql://postgres:postgres@localhost:5432/mysite",
+        conn_max_age=600,
+    )
 }
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
