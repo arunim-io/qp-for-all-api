25696add21ec19af3b4cf0aa4db5c343d64ccf68
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-11-18 22:24:14 +0600

    fix csrf issues

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,9 +3,9 @@ from decouple import config
 
 from .main import ALLOWED_HOSTS
 
-SERVER_URL = config(
-    "SERVER_URL", default="https://qp-for-all-api.onrender.com"
-)
+SERVER_URL = config("SERVER_URL", default="qp-for-all-api.onrender.com")
+
+CSRF_TRUSTED_ORIGINS = [f"https://{SERVER_URL}"]
 
 ALLOWED_HOSTS += [
     SERVER_URL,
